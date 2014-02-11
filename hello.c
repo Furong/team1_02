fa591cc922317ffa273960da93cd07ed9debf38c
mweser <matthew.weser@gmail.com>
2014-02-11 09:53:38 -0700

    Revert "Update hello.c"
    This reverts commit ebf84dc47983c139315e8325aa40cdb58b8d0a09.

--- hello.c
+++ hello.c
@@ -3,9 +3,9 @@
 int main() {
   printf("Hello world.\n");
   printf("This is Matt.\n");
-  printf("This is Anna.\n");
 
   //Turn comment with your name to ACTIVE to show you edited the file.
+  //  printf("This is Anna.\n");
   //printf("This is Furong.\n");
   return 0;
 }
