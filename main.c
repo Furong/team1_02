a2ccb1634a1ec3a7eba8f4a0a61307b0bc871dc4
mweser <matthew.weser@gmail.com>
2014-02-12 20:08:31 -0700

    Added some comments
    Comments added to some segments to better describe what will be
    happening in each function.

--- main.c
+++ main.c
@@ -6,8 +6,14 @@ int main (int argc, const char *argv[]) {
     FILE *source_file;
     char source_name[MAX_FILE_NAME_LENGTH];
     char date[DATE_STRING_LENGTH];
-    
-   /* Missing Code Here */
+   
+   /* Missing Code Here
+	pass source_file to init_lister()
+	
+	use a while loop to go through all lines of the code being read
+	
+	
+	*/
     return 0;
 }
 
@@ -15,7 +21,11 @@ FILE *init_lister(const char *name, char source_file_name[], char dte[]) {
     time_t timer;
     FILE *file;
     
-    /* Missing Code Here */
+    /* Missing Code Here 
+	copy the name into variable called “source_name”, open the source file parameter “source_file”, and set the date and time string.
+	 
+	 
+	 */
     return file;
 }
 
