5f6c714ea250c8db32cec84c12cac73df24c53d7
Matthew Weser <matthew.weser@gmail.com>
2014-02-12 20:20:18 -0700

    Added print line to test
    I'm testing to see what happens when I edit the file online and try changing it.

--- hello.c
+++ hello.c
@@ -4,6 +4,7 @@ int main() {
   printf("Hello world.\n");
   printf("This is Matt.\n");
   printf("This is Anna.\n");
+  printf("I'm testing editing your fork of the project.");
 
   //Turn comment with your name to ACTIVE to show you edited the file.
 
